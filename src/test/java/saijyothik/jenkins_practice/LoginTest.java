6a3f9ce7a42614486fca68839b47c6dd830845d3
k saijyothi <kuchipudisaijyothi@gmail.com>
2026-05-11 21:30:46 +0530

    firstcommit

---
+++ src/test/java/saijyothik/jenkins_practice/LoginTest.java
@@ -0,0 +1,13 @@
+package saijyothik.jenkins_practice;
+import org.testng.annotations.*;
+public class LoginTest {
+@Test
+	void hello()
+{
+		System.out.println("LOGIN TEST IS WORKING *******");
+	}
+	
+
+	}
+
+
